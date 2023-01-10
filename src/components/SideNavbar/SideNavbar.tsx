bb9489dc654d2646f90106a9d1d889ca4b64ddb1
Tyrone09876543 <rubik09876543@gmail.com>
2023-01-09 21:10:46 -0400

    working on sidebar styles

--- src/components/SideNavbar/SideNavbar.tsx
+++ src/components/SideNavbar/SideNavbar.tsx
@@ -1,7 +1,7 @@
 import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse,faGraduationCap, faBriefcase, faFolder, faPhoneVolume } from '@fortawesome/free-solid-svg-icons'
-import { SidebarStyle } from './styles'
+import { SidebarStyle, MenuStyle, MenuItemStyle } from './styles'
 
 
 const SideNavbar = ()=> {
@@ -10,12 +10,15 @@ const SideNavbar = ()=> {
         customBreakPoint="500px"
         transitionDuration={400}
         backgroundColor="var(--black-ww)"
-        // rootStyles={SidebarStyle}
+        rootStyles={SidebarStyle}
         >
-            <Menu rootStyles={SidebarStyle}>
+            <Menu 
+            rootStyles={MenuStyle}
+            menuItemStyles={MenuItemStyle}
+            >
                 <MenuItem icon={<FontAwesomeIcon icon={faHouse}/>} href="#sect" >Home</MenuItem>
                 <MenuItem icon={<FontAwesomeIcon icon={faGraduationCap} />}>Estudios</MenuItem>
-                <MenuItem icon={<FontAwesomeIcon icon={faBriefcase} />}>Experiencia laboral</MenuItem>
+                <MenuItem icon={<FontAwesomeIcon icon={faBriefcase} />}>Experiencia</MenuItem>
                 <MenuItem icon={<FontAwesomeIcon icon={faFolder} />}>Proyectos</MenuItem>
                 <MenuItem icon={<FontAwesomeIcon icon={faPhoneVolume} />}>Contactame</MenuItem>
             </Menu>
--- src/components/SideNavbar/styles.ts
+++ src/components/SideNavbar/styles.ts
@@ -1,6 +1,24 @@
 interface Styles {
-    [key: string]: string;
+    [key: string]: any;
 }
 export const SidebarStyle: Styles = {
-    height: '100%'
+    borderRight: "0px",
+}
+export const MenuStyle: Styles = {
+    //height: '100%',
+    //backgroundColor: 'var(--black-w)',
+}
+export const MenuItemStyle: Styles = {
+      button: ({ active }: {active: boolean}) => {
+        return {
+            fontSize: '18px',
+            color: 'var(--white)',
+            transition: 'all 0.3s ease-out',
+            '&:hover': {
+                color: '#0096FF',
+                backgroundColor: 'var(--black-w)',
+            },
+            //backgroundColor: active ? 'var(--black-w)' : undefined,
+          };
+      },
 }
