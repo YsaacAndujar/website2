98c52f6fb8c3639d00f761c5a9bc19c4e655a826
Tyrone09876543 <rubik09876543@gmail.com>
2023-02-08 19:00:43 -0400

    starting api context

---
+++ src/context/LanguageContext.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, createContext, useState } from "react";
+export const LanguageContext = createContext({});
+
+interface Props {
+  children: ReactNode;
+}
+
+type LangageType = {
+  Language: string;
+  setLanguage: (value: string) => void;
+};
+
+export const LanguageContextProvider = ({ children }: Props) => {
+  const [Language, setLanguage] = useState<string>("es");
+  return (
+    <LanguageContext.Provider value={{ Language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
