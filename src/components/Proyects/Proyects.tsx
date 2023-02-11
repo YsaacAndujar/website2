03c915af416091814617fd2e4ba51594c6c15980
Tyrone09876543 <rubik09876543@gmail.com>
2023-02-11 13:14:24 -0400

    translation done

--- src/components/Proyects/Proyects.tsx
+++ src/components/Proyects/Proyects.tsx
@@ -35,90 +35,18 @@ interface proyectInterface {
 const proyects: Array<proyectInterface> = [
   {
     title: {
-      es: "Recarga365",
-      en: "",
-    },
-    summary: {
-      es: [
-        'Este fue un proyecto en el que trabajé para la empresa "Seguridad Residencial y Comercial".',
-        "Aquí realicé un BackOffice y API's para una aplicación móvil sobre recargas telefónicas, transferencia de dinero y compra y              venta de tickets de lotería.",
-      ],
-      en: [],
-    },
-    technologies: [<PythonIcon />, <DjangoIcon />, <PostgreSQLIcon />],
-    more: [],
-  },
-  {
-    title: {
-      es: "Trailers (BackEnd - API)",
-      en: "",
-    },
-    summary: {
-      es: [
-        "Esta aplicación es un BackOffice para una aplicación web para ver trailers de peliculas.",
-        "Aquí podrás crear nuevas categorías y películas. También tiene varias API que permiten obtener los datos de la base de datos para poder visualizarlas en la aplicación final que tendrá el usuario.",
-      ],
-      en: [],
-    },
-    technologies: [<PythonIcon />, <DjangoIcon />, <PostgreSQLIcon />],
-    more: [],
-  },
-  {
-    title: {
-      es: "Trailers (FrontEnd)",
-      en: "",
-    },
-    summary: {
-      es: [
-        "Este es el FrontEnd de mi BackEnd de Trailers. Consta de un sistema de login de usuarios, una parte para explorar películas y home.",
-        "También puedes ver los detalles de una película y ver su tráiler en la misma página.",
-      ],
-      en: [],
-    },
-    technologies: [<AngularIcon />, <TypeScriptIcon />],
-    more: [],
-  },
-  {
-    title: {
-      es: "Super Market Web",
-      en: "",
-    },
-    summary: {
-      es: [
-        "Puedes Elegir productos y agregarlos al carrito de la compra y rastrear sus pedidos.",
-        "Los administradores pueden: Modificar los productos, categorias, ver todos los pedidos en un mapa y ver los detalles de cada uno. También pueden modificar el estado de los envíos y modificar el carrousel de la página de inicio.",
-      ],
-      en: [],
-    },
-    technologies: [<CSharpIcon />, <BlazorIcon />],
-    more: [],
-  },
-  {
-    title: {
-      es: "Pokemon Game",
-      en: "",
-    },
-    summary: {
-      es: [
-        'Inspirado en "Quien es este pokemon?", esta app te da una imagen de un pokemon y debes elegir su nombre correcto entre 4 opciones distintas.',
-        "También tiene una pokédex para investigar los pokemon desconocidos.",
-      ],
-      en: [],
-    },
-    technologies: [<VueIcon />, <JavaScriptIcon />],
-    more: [],
-  },
-  {
-    title: {
       es: "MagyNet",
-      en: "",
+      en: "MagyNet",
     },
     summary: {
       es: [
         'CRM de la empresa "Tennos Business"',
         "Aquí se administra todos los usuarios y empleados. También se manejan las solicitudes y se puede ver los KPI, gráficos, entre otras cosas. Básicamente todo lo que necesita un CRM.",
       ],
-      en: [],
+      en: [
+        'CRM of the company "Tennos Business"',
+        "Here you manage all users and employees, requests and you can see KPIs, graphs, among other things. Basically everything a CRM needs.",
+      ],
     },
     technologies: [
       <VueIcon />,
@@ -126,37 +54,146 @@ const proyects: Array<proyectInterface> = [
       <CSharpIcon />,
       <SqlServerIcon />,
     ],
-    more: [],
+    more: [<EmptyIcon />],
+  },
+  {
+    title: {
+      es: "Recarga365",
+      en: "Recarga365",
+    },
+    summary: {
+      es: [
+        'Este fue un proyecto en el que trabajé para la empresa "Seguridad Residencial y Comercial".',
+        "Aquí realicé un BackOffice y API's para una aplicación móvil sobre recargas telefónicas, transferencia de dinero y compra y venta de tickets de lotería.",
+      ],
+      en: [
+        'This was a project I worked on for the company "Residential and Commercial Security".',
+        'Here I made a BackOffice and API\'s for a mobile application about phone recharges, money transfers and buying and selling lottery tickets.'
+      ],
+    },
+    technologies: [<PythonIcon />, <DjangoIcon />, <PostgreSQLIcon />],
+    more: [<YouTubeIcon link="https://www.youtube.com/embed/Od5LRsEUXOI" />],
   },
   {
     title: {
       es: "MagyPago",
-      en: "",
+      en: "MagyPago",
     },
     summary: {
       es: [
-        'Magypago funciona con una terminal de pago Ingenico y/o Ticket que contiene la identidad del proveedor, para el uso exclusivo de consumo en un establecimiento determinado.'
-,'Todo es manejado a través de una plataforma web, donde podrá ver todos los movimientos de consumos de su establecimiento.'
+        "Magypago funciona con una terminal de pago Ingenico y/o Ticket que contiene la identidad del proveedor, para el uso exclusivo de consumo en un establecimiento determinado.",
+        "Todo es manejado a través de una plataforma web, donde podrá ver todos los movimientos de consumos de su establecimiento.",
       ],
-      en: [],
+      en: [
+        "Magypago works with an Ingenico and/or Ticket payment terminal that contains the supplier's identity, for the exclusive use of consumption in a specific establishment.",
+        "Everything is managed through a web platform, where you can see all the consumption movements of your establishment.",
+      ],
     },
     technologies: [<AspxIcon />, <CSharpIcon />, <SqlServerIcon />],
-    more: [],
+    more: [<EmptyIcon />],
+  },
+  {
+    title: {
+      es: "Pokemon Game",
+      en: "Pokemon Game",
+    },
+    summary: {
+      es: [
+        'Inspirado en "Quien es este pokemon?", esta app te da una imagen de un pokemon y debes elegir su nombre correcto entre 4 opciones distintas.',
+        "También tiene una pokédex para investigar los pokemon desconocidos.",
+      ],
+      en: [
+        'Inspired by "Who is this pokemon?", this app gives you an image of a pokemon and you must choose its correct name among 4 different options.',
+        "It also has a pokédex to research unknown pokemon.",
+      ],
+    },
+    technologies: [<VueIcon />, <JavaScriptIcon />],
+    more: [
+      <GitHubIcon link="https://github.com/YsaacAndujar/pokemon-vue2" />,
+      <YouTubeIcon link="https://www.youtube.com/embed/spfMqkFH6-g" />,
+      <LiveIcon link="https://ypokemon.netlify.app/" />,
+    ],
+  },
+  {
+    title: {
+      es: "Trailers (BackEnd - API)",
+      en: "Trailers (BackEnd - API)",
+    },
+    summary: {
+      es: [
+        "Esta aplicación es un BackOffice para una aplicación web para ver trailers de peliculas.",
+        "Aquí podrás crear nuevas categorías y películas. También tiene varias API que permiten obtener los datos de la base de datos para poder visualizarlas en la aplicación final que tendrá el usuario.",
+      ],
+      en: [
+        "This application is a BackOffice for a web application to watch movie trailers.",
+        "Here you can create new categories and movies. It also has several APIs that allow obtaining the data from the database to be able to visualize them in the final application that the user will have.",
+      ],
+    },
+    technologies: [<PythonIcon />, <DjangoIcon />, <PostgreSQLIcon />],
+    more: [
+      <GitHubIcon link="https://github.com/YsaacAndujar/Trailers_API_Django_Rest_Framework" />,
+      <YouTubeIcon link="https://www.youtube.com/embed/QSVky1ARCss" />,
+    ],
+  },
+  {
+    title: {
+      es: "Trailers (FrontEnd)",
+      en: "Trailers (FrontEnd)",
+    },
+    summary: {
+      es: [
+        "Este es el FrontEnd de mi BackEnd de Trailers. Consta de un sistema de login de usuarios, una parte para explorar películas y home.",
+        "También puedes ver los detalles de una película y ver su tráiler en la misma página.",
+      ],
+      en: [
+        "This is the FrontEnd of my Trailers BackEnd. It consists of a user login system, a part to explore movies and home.",
+        "You can also view the details of a movie and watch its trailer on the same page.",
+      ],
+    },
+    technologies: [<AngularIcon />, <TypeScriptIcon />],
+    more: [
+      <GitHubIcon link="https://github.com/YsaacAndujar/Trailers_Angular" />,
+      <YouTubeIcon link="https://www.youtube.com/embed/A618vRaX_w4" />,
+    ],
+  },
+  {
+    title: {
+      es: "Super Market Web",
+      en: "Super Market Web",
+    },
+    summary: {
+      es: [
+        "Puedes Elegir productos y agregarlos al carrito de la compra y rastrear sus pedidos.",
+        "Los administradores pueden: Modificar los productos, categorias, ver todos los pedidos en un mapa y ver los detalles de cada uno. También pueden modificar el estado de los envíos y modificar el carrousel de la página de inicio.",
+      ],
+      en: [
+        "You can Choose products and add them to the shopping cart and track your orders.",
+        "Administrators can: Modify products, categories, see all orders on a map and see the details of each one. They can also modify the status of submissions and modify the home page carousel.",
+      ],
+    },
+    technologies: [<CSharpIcon />, <BlazorIcon />],
+    more: [
+      <GitHubIcon link="https://github.com/YsaacAndujar/SuperMarketWeb" />,
+      <YouTubeIcon link="https://www.youtube.com/embed/spfMqkFH6-g" />,
+    ],
   },
   {
     title: {
       es: "Este portafolio",
-      en: "",
+      en: "This portfolio",
     },
     summary: {
       es: [
         "Versión 2 de mi portafolio.",
         "Creado para que puedan conocer un poco más sobre mis capacidades",
       ],
-      en: [],
+      en: [
+        "Version 2 of my portfolio.",
+        "Created so that you can know a little more about my capabilities",
+      ],
     },
     technologies: [<ReactIcon />, <TypeScriptIcon />],
-    more: [],
+    more: [<GitHubIcon link="https://github.com/YsaacAndujar/website2" />],
   },
 ];
 const title: KeyStr = {
@@ -178,161 +215,14 @@ const Proyects = () => {
               {summary[Language].map((text) => {
                 return <p className="text">{text}</p>;
               })}
-
               <div className="icons-container">
                 <div className="icon-group right">{technologies}</div>
                 <hr className="c-separator" />
-
-                <div className="icon-group center">
-                {more}
-                  <YouTubeIcon link="https://www.youtube.com/embed/Od5LRsEUXOI" />
-                </div>
+                <div className="icon-group center">{more}</div>
               </div>
             </div>
           );
         })}
-
-        <div className="card">
-          <h2 className="title-card">Trailers (BackEnd - API)</h2>
-          <div className="icons-container">
-            <div className="icon-group right">
-              <PythonIcon />
-              <DjangoIcon />
-              <PostgreSQLIcon />
-            </div>
-            <hr className="c-separator" />
-
-            <div className="icon-group center">
-              <GitHubIcon link="https://github.com/YsaacAndujar/Trailers_API_Django_Rest_Framework" />
-              <YouTubeIcon link="https://www.youtube.com/embed/QSVky1ARCss" />
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <h2 className="title-card">Trailers (FrontEnd)</h2>
-
-          <div className="icons-container">
-            <div className="icon-group right">
-              <AngularIcon />
-              <TypeScriptIcon />
-            </div>
-            <hr className="c-separator" />
-
-            <div className="icon-group center">
-              <GitHubIcon link="https://github.com/YsaacAndujar/Trailers_Angular" />
-              <YouTubeIcon link="https://www.youtube.com/embed/A618vRaX_w4" />
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <h2 className="title-card">Super Market Web</h2>
-          <p className="text">
-            Puedes Elegir productos y agregarlos al carrito de la compra y
-            rastrear sus pedidos.
-          </p>
-          <p className="text">
-            Los administradores pueden: Modificar los productos, categorias, ver
-            todos los pedidos en un mapa y ver los detalles de cada uno. También
-            pueden modificar el estado de los envíos y modificar el carrousel de
-            la página de inicio.
-          </p>
-          <div className="icons-container">
-            <div className="icon-group right">
-              <CSharpIcon />
-              <BlazorIcon />
-            </div>
-            <hr className="c-separator" />
-
-            <div className="icon-group center">
-              <GitHubIcon link="https://github.com/YsaacAndujar/SuperMarketWeb" />
-              <YouTubeIcon link="https://www.youtube.com/embed/spfMqkFH6-g" />
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <h2 className="title-card">Pokemon Game</h2>
-          <p className="text">
-            Inspirado en "Quien es este pokemon?", esta app te da una imagen de
-            un pokemon y debes elegir su nombre correcto entre 4 opciones
-            distintas.
-          </p>
-          <p className="text">
-            También tiene una pokédex para investigar los pokemon desconocidos.
-          </p>
-          <div className="icons-container">
-            <div className="icon-group right">
-              <VueIcon />
-              <JavaScriptIcon />
-            </div>
-            <hr className="c-separator" />
-
-            <div className="icon-group center">
-              <GitHubIcon link="https://github.com/YsaacAndujar/pokemon-vue2" />
-              <YouTubeIcon link="https://www.youtube.com/embed/spfMqkFH6-g" />
-              <LiveIcon link="https://ypokemon.netlify.app/" />
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <h2 className="title-card">MagyNet</h2>
-          <p className="text">CRM de la empresa "Tennos Business"</p>
-          <p className="text">
-            Aquí se administra todos los usuarios y empleados. También se
-            manejan las solicitudes y se puede ver los KPI, gráficos, entre
-            otras cosas. Básicamente todo lo que necesita un CRM.
-          </p>
-          <div className="icons-container">
-            <div className="icon-group right">
-              <VueIcon />
-              <JavaScriptIcon />
-              <CSharpIcon />
-              <SqlServerIcon />
-            </div>
-            <hr className="c-separator" />
-
-            <div className="icon-group center">
-              <EmptyIcon />
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <h2 className="title-card">MagyPago</h2>
-          <p className="text">Producto de la empresa "Tennos Business"</p>
-          <p className="text">Este producto ayuda a</p>
-          <p className="text">Gestión de facturas con comprobante fiscal.</p>
-          <p className="text">Gestión de linea de crédito.</p>
-          <p className="text">Administrar consumos prepagados</p>
-          <div className="icons-container">
-            <div className="icon-group right">
-              <AspxIcon />
-              <CSharpIcon />
-              <SqlServerIcon />
-            </div>
-            <hr className="c-separator" />
-
-            <div className="icon-group center">
-              <EmptyIcon />
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <h2 className="title-card">Este portafolio</h2>
-          <p className="text">Versión 2 de mi portafolio.</p>
-          <p className="text">
-            Creado para que puedan conocer un poco más sobre mis capacidades
-          </p>
-          <div className="icons-container">
-            <div className="icon-group right">
-              <ReactIcon />
-              <TypeScriptIcon />
-            </div>
-            <hr className="c-separator" />
-
-            <div className="icon-group center">
-              <EmptyIcon />
-            </div>
-          </div>
-        </div>
       </div>
       <hr className="final-separator" />
     </>
